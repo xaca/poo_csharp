d50886c47c3f5acc5a5eb631b00fd9e3a96384c0
Xaca <and1res@hotmail.com>
2020-01-10 16:56:47 -0500

    Actualizacion clases & objetos

---
+++ Panaderia/Panaderia/Galleta.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia
+{
+    class Galleta
+    {
+        private string nombre;
+        private string color;
+        private float peso;
+
+    }
+}
---
+++ Panaderia/Panaderia/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Galleta galleta;
+            galleta = new Galleta();
+
+            Console.WriteLine("Funciona el objeto");
+            Console.ReadKey();
+
+        }
+    }
+}
